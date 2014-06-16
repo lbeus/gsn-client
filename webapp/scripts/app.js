98710fa84f0d740d224b3194d3337f01dd492c3f
hrcp <hrvoje12@msn.com>
2014-06-16 20:33:38 +0200

    popravljena navigacija malo

--- webapp/scripts/app.js
+++ webapp/scripts/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var $routeProviderReference;
-
 var app = angular.module('gsnClientApp', [
+  //'ngAnimate',
   'ngCookies',
   'ngResource',
   'ngSanitize',
@@ -11,33 +10,29 @@ var app = angular.module('gsnClientApp', [
   'ui.date',
   'ngQuickDate',
   'ngGrid',
-  'NgSwitchery',
-  'gridster',
-  'multi-select'
+  'NgSwitchery'
 ]);
 
 
 app.config(function ($routeProvider, $httpProvider) {
-   $routeProviderReference = $routeProvider;
-
    $routeProvider.
       when ('/', {
           templateUrl: 'views/home.html',
           controller: 'HomeController'
       })
-      .when('/home', {
+      .when ('/home', {
           templateUrl: 'views/home.html',
           controller: 'HomeController'
       })
-     .when('/data', {
+      .when('/data', {
         templateUrl: 'views/data.html',
         controller: 'DataController'
       })
-      .when ('/map', {
+      .when ( '/map', {
           templateUrl: 'views/map.html',
           controller: 'MapController'
       })
-      /*.when('/passiveHeating', {
+      .when('/passiveHeating', {
         templateUrl: 'views/passiveHeating.html',
         controller: 'PassiveHeatingController'
       })
@@ -45,10 +40,22 @@ app.config(function ($routeProvider, $httpProvider) {
         templateUrl: 'views/relay.html',
         controller: 'RelayController'
       })
-      .when('/ipcamera', {
-        templateUrl: 'views/ipCamera.html',
-        controller: 'IPCamera'
-      })*/
+	  .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl'
+      })
+	  .when('/desc', {
+        templateUrl: 'views/modifyDesc.html',
+        controller: 'ModifyController'
+      })
+	  .when('/config', {
+        templateUrl: 'views/config.html',
+        controller: 'ConfigController'
+		})
+	  .when('/adminMain', {
+		templateUrl: 'views/adminMain.html',
+		controller: 'AdminMainCtrl'
+		})
       .otherwise({
         redirectTo: '/'
       });
@@ -59,9 +66,18 @@ app.config(function ($routeProvider, $httpProvider) {
         }
         return $.param(data);
     };
+
+    /*$httpProvider.responseInterceptors.push('myHttpInterceptor');
+
+    var spinnerFunction = function spinnerFunction(data, headersGetter) {
+      $("#spinner").show();
+      console.log('prikazi spinner');
+      return data;
+    };
+
+    $httpProvider.defaults.transformRequest.push(spinnerFunction);*/
   });
 
-/*
 app.factory('myHttpInterceptor', function ($q, $window) {
   return function (promise) {
     return promise.then(function (response) {
@@ -75,35 +91,15 @@ app.factory('myHttpInterceptor', function ($q, $window) {
     });
   };
 });
-*/
 
 
-app.run(function($rootScope, $location,$http,$route, NavigationService) {
 
-  $http.get('/routes').success(function (data) {   
-    var j = 0,
-      currentRoute;
-    for ( ; j < data.length; j++ ) {
-      currentRoute = data[j];
-      $routeProviderReference.when(currentRoute.name, {
-        templateUrl: currentRoute.templateUrl,
-        controller: currentRoute.controller
-      });
-
-      NavigationService.addPage(
-        {
-          pageName:currentRoute.pageName, 
-          url:currentRoute.url, 
-          active:currentRoute.active
-        }
-      );
-    }
-    $route.reload();
-
-  });
+app.run(function($rootScope, $location, NavigationService) {
 
     $rootScope.$on('$routeChangeStart', function(next, current) { 
+
          NavigationService.pageChanged($location.path());
+    
     });
 });
 
