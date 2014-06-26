8c38b02dcd890d75ce780b2f696939b68c9f932b
Marta Milakovic <marta.milakovic@gmail.com>
2014-06-26 11:52:26 +0200

    Adding Electricity Meter driver

--- webapp/scripts/services/ChartService.js
+++ webapp/scripts/services/ChartService.js
@@ -39,7 +39,7 @@ angular.module('gsnClientApp')
       for (var j = 0; j < nValues; j++)
       {
         myData.push({
-          name: name,
+          name: valueNames[j],
           data: allData[j],
           type: type,
           dataLabels: valueNames[j],
